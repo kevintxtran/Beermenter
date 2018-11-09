7ac959af5110eacad9c4f25858e1f02ff28dbaca
EtsuN <etnakaha@ucsd.edu>
2018-11-09 08:58:36 +0800

    login brewery css mostly done

--- js/index.js
+++ js/index.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
 	initializePage();
+	$('input#confirm').hide();	
 });
-/*
-function loadNav(){
-    $("#included_nav").load('navigation.html'); 
-}
-*/
-function initializePage() {
 
+function initializePage() {
+/*
 	$("#signup").click(function () {
 		var username = $('#username').val();
 		var password = $('#password').val();
@@ -27,23 +24,93 @@ function initializePage() {
 			window.alert("You're signed up! Try logging in now!");
 		}
 	});
-
-	$("#login").click(function () {
+*/
+	$("#login-signup-btn").click(function () {
 	    var username = $('#username').val();
 		var password = $('#password').val();
 		username = username.replace(/\s+/g, '');
-		password = password.replace(/\s+/g, '');
 		if (username.length == 0) {
 			window.alert("Username is empty!");
+			return;
 		}
 		else if (password.length == 0) {
 			window.alert("Password is empty!");
+			return;
 		}
-		else if (localStorage.getItem(username) != password) {
-			window.alert("Username or password is wrong.");
+		var mode = $('.switch-mode').data('switch');
+		var confirm = $('#confirm').val();
+		if (mode == "login") {
+			if (localStorage.getItem(username) != password) {
+				window.alert("Username or password is wrong.");
+			}
+			else {
+				window.location.href='breweries.html';
+			}
 		}
 		else {
-			window.location.href='breweries.html';
+			if (localStorage.getItem(username)) {
+		    	window.alert("This username has already been used!");
+			}
+			else if (confirm != password) {
+				window.alert("Password confirmation doesn't match!");
+			}
+			else {
+				localStorage.setItem(username, password);
+				window.alert("You're signed up! Try logging in now!");
+				switchMode();
+			}
 		}
 	});
+
+	$(".switch-mode").click(function() {
+		switchMode()
+	});
+
+	
+}
+
+function switchMode() {
+	var mode = $('.switch-mode').data('switch');
+	if (mode == "login") {
+		$('.switch-mode').data('switch', 'signup');
+		$('.switch-mode').fadeOut(500, function() {
+        	$(this).text('Cancel').fadeIn(500);
+    	});
+		$('.index-title').fadeOut(500, function() {
+        	$(this).text('Create Account').fadeIn(500);
+    	});
+		$('#login-signup-btn').fadeOut(500, function() {
+        	$(this).text('Sign Up').fadeIn(500);
+    	});
+    	$('#username').fadeOut(500, function() {
+        	$(this).fadeIn(500);
+    	});
+		$('#password').fadeOut(500, function() {
+        	$(this).fadeIn(500);
+    	});
+		$('.logo').fadeOut(500, function() {
+        	$('#confirm').fadeIn(500);
+    	});
+	}
+	else {
+		$('.switch-mode').data('switch', 'login');
+		$('.switch-mode').fadeOut(500, function() {
+       		$(this).text("Don't have an account? Sign up").fadeIn(500);
+    	});
+		$('.index-title').fadeOut(500, function() {
+       		$(this).text('Beermenter').fadeIn(500);
+    	});
+		$('#login-signup-btn').fadeOut(500, function() {
+       		$(this).text('Log In').fadeIn(500);
+    	});
+    	$('#username').fadeOut(500, function() {
+       		$(this).fadeIn(500);
+    	});
+		$('#password').fadeOut(500, function() {
+       		$(this).fadeIn(500);
+    	});
+		$('#confirm').fadeOut(500, function() {
+       		$('.logo').fadeIn(500);
+    	});
+	}
 }
